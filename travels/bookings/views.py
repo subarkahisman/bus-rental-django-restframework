562c7e206870a7a2aa02b43740d7276c42b22a94
subarkahisman <ismansubarkah.2210@gmail.com>
2025-05-09 08:50:35 +0700

    Add some views on views.py

--- travels/bookings/views.py
+++ travels/bookings/views.py
@@ -3,3 +3,39 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
 from rest_framework import status, generics
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Bus
+from .serializers import UserRegisterSerializer
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status = status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get9('password')
+        user = authenticate(username=username, password=password)
+
+        if user :
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user_id': user.id
+            }, status=status.HTTP_200_OK)
+        
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+
+class BusListCreateView(generics.ListCreateAPIView):
+    queryset = Bus.objects.all()
+    
